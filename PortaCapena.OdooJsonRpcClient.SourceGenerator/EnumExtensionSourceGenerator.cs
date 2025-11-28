f0907274031e03fb95157b3d7e1b97ff245e01df
Robin Noack <robin@unicorn-development.de>
2025-11-28 12:31:50 +0100

    Replace RuntimeReflexion with SourceGenerator

---
+++ PortaCapena.OdooJsonRpcClient.SourceGenerator/EnumExtensionSourceGenerator.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Text;
+
+namespace PortaCapena.OdooJsonRpcClient.SourceGenerator;
+
+[Generator]
+public class EnumExtensionSourceGenerator : ISourceGenerator
+{
+    public void Initialize(GeneratorInitializationContext context)
+    {
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+        var compilation = context.Compilation;
+
+        var sb = new StringBuilder();
+        sb.AppendLine("using System;");
+        sb.AppendLine("namespace PortaCapena.OdooJsonRpcClient.Extensions {");
+        sb.AppendLine("    public static partial class EnumExtension {");
+
+        foreach (var tree in compilation.SyntaxTrees)
+        {
+            var semantic = compilation.GetSemanticModel(tree);
+            var nodes = tree.GetRoot().DescendantNodes()
+                .OfType<EnumDeclarationSyntax>();
+
+            foreach (var enumDecl in nodes)
+            {
+                if (semantic.GetDeclaredSymbol(enumDecl) is not INamedTypeSymbol enumSymbol)
+                    continue;
+
+                sb.AppendLine($"        public static string Description(this {enumSymbol.ToDisplayString()} value)");
+                sb.AppendLine("        {");
+                sb.AppendLine("            switch (value)");
+                sb.AppendLine("            {");
+
+                foreach (var m in enumSymbol.GetMembers()
+                             .OfType<IFieldSymbol>()
+                             .Where(m => m.HasConstantValue))
+                {
+                    var attr = m.GetAttributes()
+                        .FirstOrDefault(a => a.AttributeClass?.ToString() == "System.ComponentModel.DescriptionAttribute");
+
+                    if (attr?.ConstructorArguments.First().Value is string desc)
+                    {
+                        sb.AppendLine(
+                            $"                case {enumSymbol.ToDisplayString()}.{m.Name}: return \"{desc}\";");
+                    }
+                }
+
+                sb.AppendLine("                default: return value.ToString();");
+                sb.AppendLine("            }");
+                sb.AppendLine("        }");
+            }
+        }
+
+        sb.AppendLine("    }");
+        sb.AppendLine("}");
+
+        context.AddSource("EnumDescriptionExtensions.g.cs", sb.ToString());
+    }
+}
--- PortaCapena.OdooJsonRpcClient/Extensions/EnumExtension.cs
+++ PortaCapena.OdooJsonRpcClient/Extensions/EnumExtension.cs
@@ -1,33 +1,10 @@
-﻿using Newtonsoft.Json;
-using PortaCapena.OdooJsonRpcClient.Converters;
+﻿using PortaCapena.OdooJsonRpcClient.Converters;
 using System;
-using System.ComponentModel;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
 
 namespace PortaCapena.OdooJsonRpcClient.Extensions
 {
-    public static class EnumExtension
+    public static partial class EnumExtension
     {
-        public static string Description(this Enum value)
-        {
-            try
-            {
-                FieldInfo fi = value.GetType().GetField(value.ToString());
-
-                DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(
-                    typeof(DescriptionAttribute), false);
-
-                if (attributes != null && attributes.Length > 0) return attributes[0].Description;
-                else return value.ToString();
-            }
-            catch (Exception)
-            {
-                return value.ToString();
-            }
-        }
-
         public static string OdooValue(this Enum value)
         {
             var type = value.GetType().GetField(value.ToString());
