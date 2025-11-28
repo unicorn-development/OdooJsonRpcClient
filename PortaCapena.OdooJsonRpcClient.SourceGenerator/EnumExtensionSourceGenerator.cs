2ac640bd5329cf118fd747acdd9146526616fc19
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2025-11-28 17:28:32 +0100

    (Maintenance 12 25) Replace RuntimeReflection with SourceGenerator (#7)
    * Replace RuntimeReflexion with SourceGenerator
    
    * fix problems pointed out by copilot
    
    * fix typo in DescriptionOldReflection method name

---
+++ PortaCapena.OdooJsonRpcClient.SourceGenerator/EnumExtensionSourceGenerator.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Text;
+
+namespace PortaCapena.OdooJsonRpcClient.SourceGenerator;
+
+[Generator]
+public class EnumExtensionSourceGenerator : ISourceGenerator
+{
+    public void Initialize(GeneratorInitializationContext context)
+    {
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+        var compilation = context.Compilation;
+
+        var sb = new StringBuilder();
+        sb.AppendLine("using System;");
+        sb.AppendLine("namespace PortaCapena.OdooJsonRpcClient.Extensions {");
+        sb.AppendLine("    public static partial class EnumExtension {");
+
+        foreach (var tree in compilation.SyntaxTrees)
+        {
+            var semantic = compilation.GetSemanticModel(tree);
+            var nodes = tree.GetRoot().DescendantNodes()
+                .OfType<EnumDeclarationSyntax>();
+
+            foreach (var enumDeclaration in nodes)
+            {
+                if (semantic.GetDeclaredSymbol(enumDeclaration) is not INamedTypeSymbol enumSymbol)
+                    continue;
+
+                sb.AppendLine($"        public static string Description(this {enumSymbol.ToDisplayString()} value)");
+                sb.AppendLine("        {");
+                sb.AppendLine("            switch (value)");
+                sb.AppendLine("            {");
+
+                foreach (var enumField in enumSymbol.GetMembers()
+                             .OfType<IFieldSymbol>()
+                             .Where(m => m.HasConstantValue))
+                {
+                    var attribute = enumField.GetAttributes()
+                        .FirstOrDefault(a => a.AttributeClass?.ToString() == "System.ComponentModel.DescriptionAttribute");
+
+                    var constructorArguments = attribute?.ConstructorArguments;
+
+                    if (constructorArguments?.IsDefaultOrEmpty ?? true)
+                        continue;
+
+                    if (constructorArguments.Value.First().Value is string description)
+                    {
+                        var classPath = enumSymbol.ToDisplayString();
+                        sb.AppendLine($"                case {classPath}.{enumField.Name}: return \"{description}\";");
+                    }
+                }
+
+                sb.AppendLine("                default: return value.ToString();");
+                sb.AppendLine("            }");
+                sb.AppendLine("        }");
+            }
+        }
+
+        sb.AppendLine("    }");
+        sb.AppendLine("}");
+
+        context.AddSource("EnumDescriptionExtensions.g.cs", sb.ToString());
+    }
+}
---
+++ PortaCapena.OdooJsonRpcClient.Tests/OdooEnumExtensionTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+using PortaCapena.OdooJsonRpcClient.Consts;
+using PortaCapena.OdooJsonRpcClient.Extensions;
+using Xunit;
+
+namespace PortaCapena.OdooJsonRpcClient.Tests;
+
+public class OdooEnumExtensionTests
+{
+    public static string DescriptionOldReflection(Enum value)
+    {
+        try
+        {
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(
+                typeof(DescriptionAttribute), false);
+
+            if (attributes != null && attributes.Length > 0) return attributes[0].Description;
+            else return value.ToString();
+        }
+        catch (Exception)
+        {
+            return value.ToString();
+        }
+    }
+    
+    [Fact]
+    public void TestOdooDescriptionExtension()
+    {
+        var enumValues = typeof(OdooOperator).GetEnumValues();
+        foreach (OdooOperator enumValue in enumValues)
+        {
+            Assert.Equal(enumValue.Description(), DescriptionOldReflection(enumValue));
+        }
+    }
+}
